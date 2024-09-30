bc7d80ef3c0e0f98406f3537922f2e59d25f563c
Javi H. Gil <javihgil@softspring.es>
2024-09-30 23:03:18 +0200

    Fix api.js file location

--- assets/scripts/admin/api.js
+++
@@ -1,2 +0,0 @@
-import './auto-translate';
-console.log('This file is deprecated, please use auto-translate.js instead');
---
+++ assets/scripts/api.js
@@ -0,0 +1,2 @@
+import './admin/auto-translate';
+console.log('This file is deprecated, please use admin/auto-translate.js instead');
