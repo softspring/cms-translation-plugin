7dd794e5fd27d5a40f5a2fa2a056617cd1d45601
ebpe <ebelmonte82@gmail.com>
2024-09-30 10:39:40 +0200

    Add dropdown to translations toggler

---
+++ assets/scripts/admin/toggler-translations.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', _init);
+
+function _init() {
+    const translateButtons = document.querySelectorAll('[data-locale-trans-toggler]')
+    translateButtons.forEach(function (translateButton) {
+        translateButton.addEventListener('click', function (event) {
+            togglerTranslations(translateButton);
+        });
+    });
+}
+
+function togglerTranslations(translateButton) {
+    const targetLocale = translateButton.getAttribute('data-locale-trans-toggler');
+    document.querySelectorAll('[data-locale-trans]').forEach(function (row) {
+        const locale = row.getAttribute('data-locale-trans');
+
+        if(locale == targetLocale) {
+            row.classList.remove('d-none');
+        } else {
+            row.classList.add('d-none');
+        }
+    });
+}
